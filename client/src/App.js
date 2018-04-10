b683dae2fd508aa52a545801644f72908349e584
ryandwalters <ryandwalters@gmail.com>
2018-04-10 03:14:15 -0500

    search working

--- client/src/App.js
+++ client/src/App.js
@@ -1,109 +1,164 @@
 import React, { Component } from 'react';
-import './App.css'
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      news: [{ _id: 1, name: 'news one' }, { _id: 2, name: 'news two' }]
+      articles : []
     }
-
-    //if you didn't use arrows for the submitBook
-    //this.submitBook = this.submitBook.bind(this);
   }
 
-  componentDidMount() {
-    // loadBooks()
-    //   .then(resultingJSON => this.setState({books : resultingJSON}))
+  searchNyTimes = (ev) => {
+    ev.preventDefault();
+    let query = (ev.target.children[0].children[1].value)
+    // let numResults = (ev.target.children[1].children[1].value)
+    let startDate = (ev.target.children[1].children[1].value)
+    let endDate = (ev.target.children[2].children[1].value)
 
-    this.loadNews()
-      .then(news => this.setState({ news }))
-
-    //classic js
-    // $.ajax({
-    //   action: 'localhost:3001/books',
-    // }).done(function(res){
-    //   this.setState({books: res})
-    // })
+    //make a fetch call to app.get('/articles/:q/:begin_date/:end_date/', function (req, res) {
+      
+    fetch(`http://localhost:3001/articles/${query}/${startDate}/${endDate}`)
+    .then(res => res.json())
+    .then((res) => {
+      const articles = res.response.docs;
+      
+      this.setState({articles});
+    }); 
   }
 
-  loadNews = () => {
-    return fetch("http://localhost:3001/news")
-      .then(res => res.json())
-  };
+  componentDidMount
 
-  delete = (event) => {
-    event.preventDefault();
+  
 
-    alert("delete pressed");
-    const idDelete = event.target.getAttribute('data-id');
-  };
+  render (){
+    const divStyle = {
+      'background-color': '#20315A',
+      'color': 'white'
+    };
 
-
-  submitBook = (event) => {
-    event.preventDefault();
-
-    const name = event.target.children[0].value;
-
-    // {name: 'to kill a mockingbird'} 
-
-    //instead of alerting the book name, make a fetch call to the post route in express and submit the book
-    fetch("http://localhost:3001/newsinsert", {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ name })
-    }).then(res => res.json())
-      .then((res) => {
-        this.setState({ books: [...this.state.books, res] })
-
-      });
-
-    //after it's submitted, update books in state of this component
-
-    //11:50
-  }
-
-  render() {
     return (
-      <div className="App">
-        hello world
+      <div className="container">
 
-        {/*make a form here that takes in a book and when you submit the form, alert what they put in there*/}
-        <form onSubmit={this.submitStory}>
-          <input type="text" placeholder="news story" />
-
-          <input type="submit" value="News Topic" />
-        </form>
-
-        <form onSumbit={this.deleteNews}>
-          {/* // //delete btton appears  */}
-          {this.state.news.map((n) =>
-            <p key={n._id}>
-              {n._id} <br />
-              {n.name}
-              <button data-id={n._id} onClick={this.deleteNews}> Delete Story </button>
-            </p>
-    
-                    )}
-      </form>
+      {/* <!-- Jumbotron for Title --> */}
+      <div className="jumbotron" style={divStyle}>
+        <h1 className="text-center"><strong><i className="fa fa-newspaper-o"></i> New York Times Search</strong></h1>
       </div>
-        );
-      }
-    }
-  export default App;
-  //     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
+  
+      {/* <!-- Row for git@github.com:ryandwalters/nyt-react-api.gitSearching New York Times --> */}
+       <div className="row">
+        <div className="col-sm-12">
+          <br />
+          {/* <!-- First panel is for handling the search parameters --> */}
+          <div className="panel panel-primary">
+            <div className="panel-heading">
+              <h3 className="panel-title"><strong><i className="fa  fa-list-alt"></i>   Search Parameters</strong></h3>
+            </div>
+            <div className="panel-body">
+  
+              {/* <!-- Here we create an HTML Form for handling the inputs--> */}
+              <form onSubmit={this.searchNyTimes}>
+  
+                {/* <!-- Here we create the text box for capturing the search term--> */}
+                <div className="form-group">
+                  <label for="search">Search Term:</label>
+                  <input type="text" className="form-control" id="search-term" />
+                </div>
+  
+  
+                {/* <!-- Here we capture the Start Year Parameter--> */}
+                <div className="form-group">
+                  <label for="start-year">Start Year:</label>
+                  <input type="text" className="form-control" id="start-year" />
+                </div>
+  
+                {/* <!-- Here we capture the End Year Parameter --> */}
+                <div className="form-group">
+                  <label for="end-year">End Year:</label>
+                  <input type="text" className="form-control" id="end-year" />
+                </div>
+  
+                {/* <!-- Here we have our final submit button --> */}
+                <button type="submit" className="btn btn-default" id="run-search"><i className="fa fa-search"></i> Search</button> 
+                {/* <button type="button" className="btn btn-default" id="clear-all"><i className="fa fa-trash"></i> Clear Results</button> */}
 
+              </form>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+      {/* <!-- This row will handle all of the retrieved articles --> */}
+      <div className="row">
+        <div className="col-sm-12">
+          <br />
+      
+          {/* <!-- This panel will initially be made up of a panel and wells for each of the articles retrieved --> */}
+          <div className="panel panel-primary">
+      
+            {/* <!-- Panel Heading for the retrieved articles box --> */}
+            <div className="panel-heading">
+              <h3 className="panel-title"><strong><i className="fa fa-table"></i>   Top Articles</strong></h3>
+      
+            </div>
+      
+            {/* <!-- This main panel will hold each of the resulting articles --> */}
+            <div className="panel-body" id="well-section">
+                  { /* <button type="submit" className="btn btn-default" id="run-search"><i className="fa fa-search"></i> Search</button> */ }
+                  {this.state.articles.map((art) =>
+                    <p>
+                      <h3>{art.headline.main} </h3> <br />
+                      {art.snippet} <br />
+                      {art.web_url} <br />
+                      {"Date stamp: " + art.pub_date} 
+                      {/* <button onClick={(art) => updateArticle(art)}>update</button> */}
+                        
+                        {/* updateBook = (art) => { */}
+                               {/* alert(id)} */}
+                    </p>
+                    
+                  )}
+      
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="row">
+          <div className="col-sm-12">
+            <br />
+      
+            {/* <!-- This panel will initially be made up of a panel and wells for each of the articles saved --> */}
+            <div className="panel panel-primary">
+      
+              {/* <!-- Panel Heading for the retrieved articles box --> */}
+              <div className="panel-heading">
+                <h3 className="panel-title"><strong><i className="fa fa-table"></i>   Saved Articles</strong></h3>
+            </div>
+      
+              {/* <!-- This main panel will hold each of the resulting articles --> */}
+              <div className="panel-body" id="well-section">
+                  {/* <button type="submit" className="btn btn-default" id="run-search"><i className="fa fa-search"></i> Search</button> */}
+      
+              </div>
+            </div>
+          </div>
+        </div>
+      
+      {/* <!-- Footer Region --> */}
+      <div className="row">
+        <div className="col-sm-12">
+      
+          {/* <!-- Line Break followed by closing --> */}
+          <hr />
+          <h5 className="text-center"><small><i className="fa fa-heart"></i></small></h5>
+      
+        </div>
+      </div>
+    </div>
+    );
+
+    }
+  }
+
+export default App;
--- server.js
+++ server.js
@@ -1,29 +1,27 @@
-// Dependencies
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const axios =require ("axios")
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require("./routes/apiRoutes.js");
+const request = require("request");
+// const apiRoutes = require("./routes/apiRoutes.js");
 const mongojs = require("mongojs");
-
-// Set the app up with morgan
 app.use(logger("dev"));
-
-// set the app up with bodyparser
 app.use(bodyParser());
+
+// const scraper = require("./utils/api.js");
+
 
 // Database configuration
 const databaseUrl = process.env.MONGODB_URI || "nytreact_db";
-const collections = ["news"];
+const collections = ["articles"];
 
 // Hook mongojs config to db variable
-const db = mongojs(databaseUrl , collections);
+const db = mongojs(databaseUrl, collections);
 
 // Log any mongojs errors to console
-db.on("error", function(error) {
+db.on("error", function (error) {
   console.log("Database Error:", error);
 });
 
@@ -34,64 +32,135 @@ db.on("error", function(error) {
 
   read up on CORs here: https://www.maxcdn.com/one/visual-glossary/cors/
 */
-  //allow the api to be accessed by other apps
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
-    next();
+//allow the api to be accessed by other apps
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+  next();
+});
+
+// app.get('/scrape', function (req, res) {
+//   request("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+//   "b9f91d369ff59547cd47b931d8cbc56b:0:74623931" + "&q=")
+//   scraper.getArticles
+
+// });
+//api key from NYTIMES
+
+// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+// url += '?' + this.param({
+//   'api-key': "5e863d798747414bb5030449461746ce",
+//   'q': "jazz",
+//   'begin_date': "20180101",
+//   'end_date': "20180130",
+//   'page': 0
+// });
+// $.ajax({
+//   url: url,
+//   method: 'GET',
+// }).done(function (result) {
+//   console.log(result);
+// }).fail(function (err) {
+//   throw err;
+// });
+
+app.get('/articles/:q/:begin_date/:end_date/', function (req, res) {
+  var ob = {
+    'api-key': "5e863d798747414bb5030449461746ce",
+    'page': '0',
+    'q': req.params.q,
+    'begin_date': req.params.begin_date
+  }
+
+  if (req.params.end_date != 0) {
+    ob.end_date = req.params.end_date;
+  }
+
+  request.get({
+    url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+    qs: ob,
+  }, function (err, response, body) {
+    body = JSON.parse(body);
+    res.json(body);
+  })
+})
+
+//Route to get all articles
+app.get('/articles', function (req, res) {
+  db.articles.find({}, function (error, result) {
+    res.json(result);
+  });
+});
+
+app.put('/article/:id', function (req, res) {
+  db.articles.findAndModify({
+    query: {
+      "_id": mongojs.ObjectId(req.params.id)
+    },
+    update: {
+      $set: {
+        "name": req.body.name
+      }
+    },
+    new: true
+  }, function (err, editedArticle) {
+    res.json(editedArticle);
+  });
+});
+
+  //route to save an article
+  app.post('/savedArticle', function (req, res) {
+    db.articles.insert(req.body, function (error, savedArticle) {
+      // Log any errors
+      if (error) {
+        res.send(error);
+      } else {
+        res.json(savedArticle);
+      }
+    });
   });
 
-//route to get all of my books
-app.get('/news', function(req, res){
-	db.news.find({}, function(error, result){
-	    res.json(result);
-	});
-});
+  //route to delete a saved story
+  app.delete('/delete/:id', function (req, res) {
+    var article_id = req.params.id;
 
+    db.articles.remove({
+      "_id": mongojs.ObjectID(article_id)
+    }, function (error, removed) {
+      if (error) {
+        res.send(error);
+      } else {
+        res.json(article_id);
+      }
+    });
 
-//route to save a story
-app.post('/newsInsert', function(req, res){
-	db.news.insert(req.body, function(error, savedStory) {
-	  // Log any errors
-	  if (error) {
-	    res.send(error);
-	  }else {
-	    res.json(savedBook);
-	  }
-	});
-});
+  });
 
-//route to delete a saved story
-app.delete('/delete/:id', function(req, res){
-  var book_id = req.params.id;
-
-  db.news.remove({
-    "_id": mongojs.ObjectID(book_id)
-  }, function(error, removed) {
-    if (error) {
-      res.send(error);
-    }else {
-      res.json(book_id);
+  app.get('/articles/:q/:begin_date/:end_date/', function (req, res) {
+    var ob = {
+      'api-key': "bc5f77268f204de5bdbc933ba1f5c699",
+      'page': '0',
+      'q': req.params.q,
+      'begin_date': req.params.begin_date
     }
-  });
 
-});
+    if (req.params.end_date != 0) {
+      ob.end_date = req.params.end_date;
+    }
 
-// // Listen on port 3001
-  app.listen(PORT, function() {
+    request.get({
+      url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+      qs: ob,
+    }, function (err, response, body) {
+      body = JSON.parse(body);
+      res.json(body);
+    })
+  })
+
+
+
+  // // Listen on port 3001
+  app.listen(PORT, function () {
     console.log('🌎 ==> Now listening on PORT %s! Visit http://localhost:%s in your browser!', PORT, PORT);
   });
-
-
-
-
-  // app.get("/news", (req, res) => {
-  //   axios
-  //     .get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
-  //     b9f91d369ff59547cd47b931d8cbc56b:0:74623931+ "&q=", { params: req.query })
-  //     .then(({ data: { results } }) => res.json(results))
-  //     .catch(err => res.status(422).json(err));
-  // });
-  
-  // module.exports = router;
